fba8731c37e23997dbd0fbb86f75b9d807ded826
GoodKid <GoodKid88@yandex.ru>
2022-08-08 11:44:34 +0300

    add task3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,64 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.example.model.Principal;
+import org.example.model.School;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Principal.class)
+                .addAnnotatedClass(School.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Principal principal = session.get(Principal.class,1);
+            School school = session.get(School.class, 1);
+
+         //   school.setPrincipal(principal);
+//            Principal principal1 = new Principal("ann", 99);
+//            session.save(principal1);
+
+            principal.setSchool(school);
+            session.save(principal);
+
+
+
+
+//            Director director = session.get(Director.class, 1);
+//            Movie movie = session.get(Movie.class, 12);
+//
+//            movie.getDirector().getMovies().remove(movie);
+//
+//            System.out.println(director.getMovies());
+//            movie.setDirector(director);
+//            director.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+
+          //  session.save(director);
+
+
+//            Movie movie = session.get(Movie.class, 15);
+//            session.remove(movie);
+        //    Director director = session.get(Director.class, 7);
+//
+//            List<Movie> list = director.getMovies();
+//            for (Movie movie : list) {
+//                list.remove(movie);
+//            }
+//
+//            director.getMovies().clear();
+         //   session.remove(director);
+          //  director.getMovies().forEach(i -> i.setDirector(null));
+
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
